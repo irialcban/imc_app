29fb4482ed8bd8028e7fe079cc19c5f1f046cf21
Iria <irialcban@alu.edu.gva.es>
2023-11-07 12:06:51 +0100

    Lógica de las CardViews hecha

--- app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
@@ -2,10 +2,54 @@ package com.pmdm.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
 class ImcCalculatorActivity : AppCompatActivity() {
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+    var isMaleSelected:Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
+
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+
+        viewMale.setOnClickListener {
+            setGenderColor()
+        }
+
+        viewFemale.setOnClickListener {
+            setGenderColor()
+        }
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgrounColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgrounColor(!isMaleSelected))
+    }
+
+    private fun getBackgrounColor(isComponentSelected: Boolean): Int {
+        val colorReference = if(isComponentSelected) {
+            R.color.bg_comp_sel
+        }else {
+            R.color.bg_comp
+        }
+
+        return ContextCompat.getColor(this,colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
     }
 }
