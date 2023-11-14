aea2c683b327a060b1e82be4f7e57ba5bbcadd07
Iria <10714629@ieselgrao.org>
2023-11-14 21:46:37 +0100

    Haciendo la parte lógica de la nueva pantalla

--- app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
@@ -1,8 +1,10 @@
 package com.pmdm.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -25,6 +27,8 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private var isAddAgeSelected:Boolean = true
     private var age:Int = 0
     private var weight:Int = 0
+    private lateinit var btnCalculate:AppCompatButton
+    companion object { const val IMC_KEY = "RESULT"}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
@@ -45,6 +49,7 @@ class ImcCalculatorActivity : AppCompatActivity() {
         tvAge = findViewById(R.id.tvAge)
         btnSubstractAge = findViewById(R.id.btnSubstractAge)
         btnAddAge = findViewById(R.id.btnAddAge)
+        btnCalculate = findViewById(R.id.btnCalculate)
     }
 
     private fun initListeners() {
@@ -81,6 +86,10 @@ class ImcCalculatorActivity : AppCompatActivity() {
         btnAddAge.setOnClickListener {
             isAddAgeSelected = true
             setAge()
+        }
+
+        btnCalculate.setOnClickListener {
+            navigate2result(calculateIMC())
         }
     }
 
@@ -121,6 +130,18 @@ class ImcCalculatorActivity : AppCompatActivity() {
         }
 
         tvAge.text = DecimalFormat("#.##").format(age)
+    }
+
+    private fun calculateIMC(): Double {
+        var height:Double = (tvHeight.text.toString().toDouble())/100
+
+        return (weight/(height*height))
+    }
+
+    private fun navigate2result(imc:Double) {
+        val intentIRA = Intent(this,ImcResultActivity::class.java)
+        intentIRA.putExtra(IMC_KEY,imc)
+        startActivity(intentIRA)
     }
 
     private fun initUI() {
--- app/src/main/java/com/pmdm/imcapp/ImcResultActivity.kt
+++ app/src/main/java/com/pmdm/imcapp/ImcResultActivity.kt
@@ -1,11 +1,52 @@
 package com.pmdm.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import com.pmdm.imcapp.ImcCalculatorActivity.Companion.IMC_KEY
 
 class ImcResultActivity : AppCompatActivity() {
+    private lateinit var tvTextResult:TextView
+    private lateinit var tvResult:TextView
+    private lateinit var tvDescription:TextView
+    private lateinit var btnReCalculate:AppCompatButton
+    private val imc = intent.extras?.getDouble(IMC_KEY)?:-1.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_result)
+
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    private fun initComponents() {
+        tvTextResult = findViewById(R.id.tvTextResult)
+        tvResult = findViewById(R.id.tvResult)
+        tvDescription = findViewById(R.id.tvDescription)
+        btnReCalculate = findViewById(R.id.btnReCalculate)
+    }
+
+    private fun initListeners() {
+        btnReCalculate.setOnClickListener {
+            val intentICA = Intent(this,ImcCalculatorActivity::class.java)
+            startActivity(intentICA)
+        }
+    }
+
+    private fun setResult() {
+        tvResult.text = imc.toString()
+    }
+
+    private fun setDescription() {
+        when(imc) {
+            in 0.0 .. 18.5 -> tvDescription.text = getString(R.id.lower)
+        }
+    }
+
+    private fun initUI() {
+        setResult()
     }
 }
