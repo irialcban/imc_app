2c3ef09b0b3af41ce98f3d0f18f2b86cbeb3d68d
Iria <irialcban@alu.edu.gva.es>
2023-11-14 12:57:05 +0100

    Peso, Edad y Calcular hechos (Falta parte lógica) Comienzo de el ImcResultActivity

---
+++ app/src/main/java/com/pmdm/imcapp/ImcResultActivity.kt
@@ -0,0 +1,11 @@
+package com.pmdm.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_result)
+    }
+}
