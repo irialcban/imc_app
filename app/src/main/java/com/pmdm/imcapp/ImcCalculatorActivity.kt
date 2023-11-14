28fcd41483bbe1b4889f2234d094273d14b6c3ee
Iria <10714629@ieselgrao.org>
2023-11-14 19:33:17 +0100

    Parte lógica del Peso y Edad hecha

--- app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -14,6 +15,16 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private lateinit var tvHeight:TextView
     private lateinit var rsHeight:RangeSlider
     private var isMaleSelected:Boolean = true
+    private lateinit var tvWeight:TextView
+    private lateinit var btnSubstractWeight:FloatingActionButton
+    private lateinit var btnAddWeight:FloatingActionButton
+    private lateinit var tvAge:TextView
+    private lateinit var btnSubstractAge:FloatingActionButton
+    private lateinit var btnAddAge:FloatingActionButton
+    private var isAddWeightSelected:Boolean = true
+    private var isAddAgeSelected:Boolean = true
+    private var age:Int = 0
+    private var weight:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
@@ -28,6 +39,12 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubstractWeight = findViewById(R.id.btnSubstractWeight)
+        btnAddWeight = findViewById(R.id.btnAddWeight)
+        tvAge = findViewById(R.id.tvAge)
+        btnSubstractAge = findViewById(R.id.btnSubstractAge)
+        btnAddAge = findViewById(R.id.btnAddAge)
     }
 
     private fun initListeners() {
@@ -44,6 +61,26 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
         rsHeight.addOnChangeListener {_,value,_ ->
             tvHeight.text = DecimalFormat("#.##").format(value) + "cm"
+        }
+
+        btnSubstractWeight.setOnClickListener {
+            isAddWeightSelected = false
+            setWeight()
+        }
+
+        btnAddWeight.setOnClickListener {
+            isAddWeightSelected = true
+            setWeight()
+        }
+
+        btnSubstractAge.setOnClickListener {
+            isAddAgeSelected = false
+            setAge()
+        }
+
+        btnAddAge.setOnClickListener {
+            isAddAgeSelected = true
+            setAge()
         }
     }
 
@@ -62,7 +99,33 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale.setCardBackgroundColor(getBackgrounColor(!isMaleSelected))
     }
 
+    private fun setWeight() {
+        if(isAddWeightSelected) {
+            weight = weight + 1
+        }else {
+            if(weight > 0) {
+                weight = weight - 1
+            }
+        }
+
+        tvWeight.text = DecimalFormat("#.##").format(weight)
+    }
+
+    private fun setAge() {
+        if(isAddAgeSelected) {
+            age = age + 1
+        }else {
+            if(age > 0) {
+                age = age - 1
+            }
+        }
+
+        tvAge.text = DecimalFormat("#.##").format(age)
+    }
+
     private fun initUI() {
         setGenderColor()
+        setWeight()
+        setAge()
     }
 }
